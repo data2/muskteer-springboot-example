6741dbf4843277eaa3d8de77cf6f9488a8294328
MuskteerAthos <muskteerathos@gmail.com>
2019-01-11 10:52:32 +0800

    hibernate modify

--- muskteer-springboot-hibernate/src/main/java/com/muskteer/spring/boot/hibernate/DaoImpl.java
+++ muskteer-springboot-hibernate/src/main/java/com/muskteer/spring/boot/hibernate/DaoImpl.java
@@ -1,24 +1,20 @@
 package com.muskteer.spring.boot.hibernate;
 
 import com.linkage.UDPBusiness.po.UdpOrderInfo;
-import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
 
 import java.util.List;
 
 
-public class DaoImpl {
-    private HibernateTemplate hibernateTemplate;
+public class DaoImpl extends HibernateDaoSupport {
 
     public void test() {
-        System.out.println(hibernateTemplate.get(UdpOrderInfo.class, "UDP1701010000126466111").getPayorderid());
-        List<UdpOrderInfo> orders = (List<UdpOrderInfo>) hibernateTemplate.findByNamedParam(
+        System.out.println(getHibernateTemplate().get(UdpOrderInfo.class, "UDP1701010000126466111").getPayorderid());
+        List<UdpOrderInfo> orders = (List<UdpOrderInfo>) getHibernateTemplate().findByNamedParam(
                 "from UdpOrderInfo where payorderid = :payorderid",
                 new String[]{"payorderid"}, new String[]{"UDP1701010000126466111"});
         System.out.println(orders.size());
         System.out.println(orders.get(0).getServiceorderid());
     }
 
-    public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
-        this.hibernateTemplate = hibernateTemplate;
-    }
 }
